e1d1a5f62faef7df62b1e3c174d2ec3fe60b20e1
Magnus Ehlde <magnus@040.se>
2013-08-12 22:42:53 +0200

    Fixed jquery syntax

--- jquery.webkitRepaint.js
+++ jquery.webkitRepaint.js
@@ -1,8 +1,9 @@
 jQuery.fn.webkitRepaint = function () {
-  var elem = this.get(0);
-  var disp = elem.style.display;
-  elem.style.display = 'none';
-  elem.offsetHeight;
-  elem.style.display = disp;
-  return this;
-}
+  return this.each(function(){
+  	var elem = jQuery(this).get(0);
+    var disp = elem.style.display;
+    elem.style.display = 'none';
+    elem.offsetHeight;
+    elem.style.display = disp;
+  });
+};
